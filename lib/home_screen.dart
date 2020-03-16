e6cc0c00c061835db1218d57b36a3eb4183c7966
jargalbat <38040248+jargalbat@users.noreply.github.com>
2020-03-16 22:24:21 +0800

    First commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dialog/second_screen.dart';
+import 'package:flutter_dialog/spinkit_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    SpinKitScreen(),
+    SecondScreen(),
+    Text(
+      '3rd screen',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter dialog'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('2nd'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            title: Text('3rd'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/spinkit_screen.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class SpinKitScreen extends StatefulWidget {
+  @override
+  _SpinKitScreenState createState() => _SpinKitScreenState();
+}
+
+class _SpinKitScreenState extends State<SpinKitScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      padding: EdgeInsets.all(10.0),
+      itemCount: _list.length,
+      itemBuilder: (_, index) {
+        return RaisedButton(
+          child: _list[index],
+          onPressed: () {
+            _showLoading(_list[index]);
+          },
+        );
+      },
+    );
+  }
+
+  _showLoading(Widget child) {
+    showGeneralDialog(
+      context: context,
+      barrierColor: Colors.black12.withOpacity(0.6),
+      // background color
+      barrierDismissible: true,
+      // should dialog be dismissed when tapped outside
+      barrierLabel: "Dialog",
+      // label for barrier
+      transitionDuration: Duration(milliseconds: 400),
+      // how long it takes to popup dialog after button click
+      pageBuilder: (_, __, ___) {
+        // your widget implementation
+        return SizedBox.expand(
+          // makes widget fullscreen
+          child: child,
+        );
+      },
+    );
+  }
+
+  List<Widget> _list = [
+    SpinKitRotatingCircle(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitRotatingPlain(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitDoubleBounce(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitWave(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitWanderingCubes(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitFadingFour(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitFadingCube(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitPulse(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitChasingDots(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitThreeBounce(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitCircle(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitCubeGrid(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitFadingCircle(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitRotatingCircle(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitFoldingCube(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitPumpingHeart(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitDualRing(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitHourGlass(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitFadingGrid(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitRing(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitRipple(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitSpinningCircle(
+      color: Colors.white,
+      size: 50.0,
+    ),
+    SpinKitSquareCircle(
+      color: Colors.white,
+      size: 50.0,
+    ),
+  ];
+}
